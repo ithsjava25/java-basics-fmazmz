113d657457c6229153f08cfa8dea5e6874e36f6f
FMOUSSA <firas.moussa@njordengineering.se>
2025-09-16 21:31:17 +0200

    Create PriceService to deserialize Elpriser API data into internal objects

---
+++ src/main/java/com/example/service/PriceService.java
@@ -0,0 +1,29 @@
+package com.example.service;
+
+import com.example.api.ElpriserAPI;
+import com.example.model.PricePoint;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PriceService {
+  private final ElpriserAPI elpriserAPI = new ElpriserAPI();
+
+  public List<PricePoint> getPricePoints(ElpriserAPI.Prisklass zone, LocalDate date) {
+    List<PricePoint> pricePoints = new ArrayList<>();
+
+    // Try today and tomorrow instead of checking if its 1PM, fallback on today if API data is empty for tomorrow.
+    for (LocalDate d : List.of(date, date.plusDays(1))) {
+      List<ElpriserAPI.Elpris> raw = elpriserAPI.getPriser(d, zone);
+
+      // Convert API DTO's into internal DTO's as extra data fields are not required.
+      raw.forEach(r -> pricePoints.add(
+              new PricePoint(r.timeStart().toLocalDateTime(), r.sekPerKWh())
+      ));
+    }
+
+    // Return Elpris objects as internal.
+    return pricePoints;
+  }
+}
